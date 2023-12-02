8dc71f179572ddebcbe36012bf24f2e48f1c552c
AngelgMedina <152690499+AngelgMedina@users.noreply.github.com>
2023-12-02 10:49:54 +0100

    Add files via upload
    Programación de Inteligencia Artificial  Tarea para PIA01

---
+++ holamundo.py
@@ -0,0 +1,8 @@
+# Programacion de Inteligencia Artificial-Tarea para PIA01
+
+variable = "Hola Mundo"
+print(variable)
+
+"""
+https://www.mecd.es/cidead/aulavirtual/mod/assign/view.php?id=272975
+"""
